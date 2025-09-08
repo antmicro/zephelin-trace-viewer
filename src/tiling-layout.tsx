antmicro/zeppelin-trace-viewer

[#81289] tiling-layout: Adjust ID, name, resizing config
Signed-off-by: Eryk Szpotanski <eszpotanski@antmicro.com>

src/tiling-layout.tsx
@@ -126,10 +126,13 @@ export default memo(() => {
     });
 
     return (
-        <div id={style['layout-container']}>
+        <div id={style['tiling-layout']}>
             <Layout
                 model={model}
-                factory={factory} />
+                factory={factory}
+                popoutWindowName="Zeppelin Trace Viewer"
+                realtimeResize={true}
+            />
         </div>
     );
 // Override props comparison to avoid unnecessary reloads

antmicro/zephelin-trace-viewer

src/tiling-layout.tsx
@@ -126,10 +126,13 @@ export default memo(() => {
     });
 
     return (
-        <div id={style['layout-container']}>
+        <div id={style['tiling-layout']}>
             <Layout
                 model={model}
-                factory={factory} />
+                factory={factory}
+                popoutWindowName="Zephelin Trace Viewer"
+                realtimeResize={true}
+            />
         </div>
     );
 // Override props comparison to avoid unnecessary reloads
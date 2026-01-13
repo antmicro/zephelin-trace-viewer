0b78ebc556830c9435e68b91c01f2f3cf5d7442f
Jedrzej Czykier <jczykier@antmicro.com>
2026-01-13 11:13:18 +0100

    [#89371] utils: Implement Cache

---
+++ src/utils/cache.ts
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2026 Analog Devices, Inc.
+ * Copyright (c) 2026 Antmicro <www.antmicro.com>
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+
+export type GroupDataStore = Record<string, Record<string, any>>;
+
+let _cache = {} as GroupDataStore;
+
+/**
+ * Global cache with component data for different groups.
+ */
+export const GroupDataCache = {
+    getOrCreateEntry<T>(title: string, groupName: string, provider: (n: string) => T): T {
+        _cache[title] ??= {};
+
+        if (groupName in _cache[title]) {
+            return _cache[title][groupName] as T;
+        }
+
+        const entry = provider(groupName);
+        _cache[title][groupName] = entry;
+
+        return entry;
+    },
+
+    clear(): void {
+        _cache = {};
+    },
+
+};

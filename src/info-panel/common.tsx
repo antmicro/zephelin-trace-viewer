7dab0579a96532c765d033755c2b39de8c542284
Andrii Chmutov <achmutov@antmicro.com>
2026-02-09 14:05:41 +0100

    [#90749] src: info-panel, styles: Switch to custom dropdown
    Signed-off-by: Andrii Chmutov <achmutov@antmicro.com>

--- src/info-panel/common.tsx
+++ src/info-panel/common.tsx
@@ -13,10 +13,16 @@
 import styles from "@styles/info-panel.module.scss";
 import { VNode } from "preact";
 import { getGroupNames } from "@speedscope/app-state/utils";
-import { useMemo, useState } from "preact/hooks";
-import { TilingComponent } from "@/utils/tiling-component";
-import { CirclePlusIcon, CloseIcon } from "@/icons";
+import { useEffect, useMemo, useRef, useState } from "preact/hooks";
+import ChevronDownIcon from "@speedscope/views/icons/chevron-down";
+import { useTheme } from "@speedscope/views/themes/theme";
+import { getStyle } from "@speedscope/views/profile-select";
+import { css } from "aphrodite";
+import { MouseEvent } from "preact/compat";
 import { getCSSColorByIdx } from "@/plots/utils";
+import { CirclePlusIcon, CloseIcon } from "@/icons";
+import { TilingComponent } from "@/utils/tiling-component";
+import { css as joinCss } from "@/utils/css";
 
 interface PanelTemplateProps {
     /** Children embedded in the panel */
@@ -29,7 +35,7 @@ interface PanelTemplateProps {
     allowGroupSelection?: boolean,
     /** Toggle to allow multiple plots */
     allowMultiplePlots?: boolean,
-    /** Togle to display legend dot */
+    /** Toggle to display legend dot */
     toggleDot?: boolean,
 
 }
@@ -43,37 +49,106 @@ interface SourceSelectorProps {
     toggleDot?: boolean;
 }
 
-const SourceSelector = ({ value, options, onUpdate, onRemove, color, toggleDot }: SourceSelectorProps) => (
-    <div className={styles["selector-container"]}>
-        {toggleDot && (
-            <span
-                className={styles["legend-indicator"]}
-                style={{ backgroundColor: color}}
-            />
-        )}
-        <select
-            className={styles["group-select"]}
-            value={value}
-            onChange={(e) => onUpdate((e.target as HTMLSelectElement).value)}
-        >
-            {options.map((name) => (
-                <option key={name} value={name}>
-                    {name}
-                </option>
-            ))};
-        </select>
-        {onRemove && (
-            <button
-                type="button"
-                className={styles["remove-button"]}
-                onClick={onRemove}
-                title="Remove source"
+const SourceSelector = ({ value, options, onUpdate, onRemove, color, toggleDot }: SourceSelectorProps) => {
+    const [profileSelectShown, setProfileSelectShown] = useState(false);
+    const groupCount = getGroupNames().length;
+    const style = getStyle(useTheme())(groupCount);
+    const ref = useRef<HTMLDivElement | null>(null);
+
+    const [hoveredOption, setHoveredOption] = useState(0);
+
+    // Close dropdown on outside click
+    useEffect(() => {
+        const handleClickOutside = (event: MouseEvent<HTMLElement>) => {
+            if (ref.current && !ref.current.contains(event.target as HTMLElement)) {
+                setProfileSelectShown(false);
+            }
+        };
+
+        document.addEventListener("pointerdown", handleClickOutside);
+        return () => document.removeEventListener("pointerdown", handleClickOutside);
+    }, [ref, setProfileSelectShown]);
+
+    // Hover selected node by default
+    useEffect(() => {
+        if (profileSelectShown) {
+            const hovered = options.indexOf(value);
+            setHoveredOption(hovered === -1 ? 0 : hovered);
+        }
+    }, [profileSelectShown]);
+
+    // Calculate the largest size of the dropdown item
+    const canvasRef = useRef<HTMLCanvasElement | null>(null);
+    const [width, setWidth] = useState(0);
+    useEffect(() => {
+        canvasRef.current ??= document.createElement("canvas");
+        const ctx = canvasRef.current.getContext("2d");
+
+        const font = ref.current && (window.getComputedStyle(ref.current)).font;
+        if (!font || !ctx) {return;}
+        ctx.font = font;
+
+        setWidth(Math.ceil(options.reduce((m, s) => {
+            const w = ctx.measureText(String(s)).width;
+            return w > m ? w : m;
+        }, 0)));
+    }, [options, ref]);
+
+    const ProfileGroupRow = (props: { name: string, index: number }) => {
+        const selected = props.name === value;
+        const hovered = props.index === hoveredOption;
+        return (
+            <div
+                className={joinCss(css(
+                    style.profileRow,
+                    selected && style.profileRowSelected,
+                    hovered && style.profileRowHovered,
+                ), styles["profile-row"])}
+                onMouseEnter={() => setHoveredOption(props.index)}
+                onClick={() => onUpdate(props.name)}
             >
-                <CloseIcon />
-            </button>
-        )}
-    </div>
-);
+                <span>{props.name}</span>
+            </div>
+        );
+    };
+
+    return (
+        <div
+            ref={ref} className={styles["selector-container"]}
+            onClick={() => setProfileSelectShown(!profileSelectShown)}
+        >
+            {toggleDot && (
+                <span
+                    className={styles["legend-indicator"]}
+                    style={{ backgroundColor: color}}
+                />
+            )}
+            <div className={styles["group-select"]} style={{ width: width }}>
+                {value}
+            </div>
+            <ChevronDownIcon up={profileSelectShown} />
+            {onRemove && (
+                <button
+                    type="button"
+                    className={styles["remove-button"]}
+                    onClick={onRemove}
+                    title="Remove source"
+                >
+                    <CloseIcon />
+                </button>
+            )}
+            <div className={styles["profile-dropdown"]} style={{display: profileSelectShown ? 'block' : 'none'}}>
+                <div className={joinCss(css(style.profileSelectOuter), styles["profile-select-outer"])}>
+                    <div className={joinCss(css(style.profileSelectBox), styles["profile-select-box"])}>
+                        <div className={css(style.profileSelectScrolling)}>
+                            {options.map((name, i) => <ProfileGroupRow name={name} index={i} />)}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
 
 /** The basic panel template */
 export default function PanelTemplate({
@@ -141,18 +216,20 @@ export default function PanelTemplate({
                             const filteredOptions = groupsPool.filter(
                                 (opt) => opt === group || !activeGroupsSt.includes(opt));
                             return (
-                                <SourceSelector
-                                    key={`${group}-${index}`}
-                                    value={group}
-                                    options={filteredOptions}
-                                    onUpdate={(val) => handleUpdateGroup(index, val)}
-                                    onRemove={canRemove
-                                        ? () => handleRemoveDropdown(index)
-                                        : undefined
-                                    }
-                                    color={groupsToColors[group]}
-                                    toggleDot={toggleDot}
-                                />
+                                <div className={styles["selector-wrapper"]}>
+                                    <SourceSelector
+                                        key={`${group}-${index}`}
+                                        value={group}
+                                        options={filteredOptions}
+                                        onUpdate={(val) => handleUpdateGroup(index, val)}
+                                        onRemove={canRemove
+                                            ? () => handleRemoveDropdown(index)
+                                            : undefined
+                                        }
+                                        color={groupsToColors[group]}
+                                        toggleDot={toggleDot}
+                                    />
+                                </div>
                             );
                         })}
                     </div>
